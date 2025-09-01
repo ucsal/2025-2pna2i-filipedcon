b994048fb9fd13d33c5098375bc6053ffa3c8fc0
lipe <liipelovemoon@gmail.com>
2025-08-31 22:33:09 -0300

    refactor: retirada scan desnecessário

--- src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
+++ src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
@@ -1,6 +1,6 @@
 package br.com.mariojp.solid.isp;
 
-public class SimplePrinter implements MultiFunctionDevice {
+public class SimplePrinter implements Scanner, Printer {
     @Override public void print(String content){ /* imprime */ }
     @Override public void scan(String targetFile){ throw new UnsupportedOperationException("Sem scanner"); }
 }
